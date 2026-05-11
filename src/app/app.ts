73eaa53fdf9593e6533a53c5a164f7030b0fc46e
LasseNP <10632094@ucn.dk>
2026-05-11 16:03:31 +0200

    First iteration of portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { Navbar } from './navbar/navbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage, MatIconModule, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Portfolio-Site');
+  gitRotated = false;
+  linkRotated = false;
+  mailLifted = false;
+  mailIcon = 'mail';
+
+  onMailEnter() {
+    this.mailLifted = true;
+    this.mailIcon = 'drafts';
+  }
+
+  onMailLeave() {
+    this.mailLifted = false;
+    this.mailIcon = 'mail';
+  }
 }
